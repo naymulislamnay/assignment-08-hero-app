b4b2c1ae3ee16b2c25263c3ca302c5ef4d6b7009
naymulislamnay <naymulislamnay@gmail.com>
2025-10-08 21:08:05 +0600

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './Navbar/Navbar'
+
+
+function App() {
+
+  return (
+
+    <div className='max-w-[1200px] mx-auto'>
+      <Navbar></Navbar>
+    </div>
+
+  )
+}
+
+export default App
